23c53ad6892cf443eef48c7ba2112a8aa871508c
NM17 <nada.mahmoud@detroitlabs.com>
2019-02-03 17:39:56 -0500

    getter/setter for QuizQuestion class

--- src/com/company/QuizQuestion.java
+++ src/com/company/QuizQuestion.java
@@ -1,4 +1,30 @@
 package com.company;
 
 public class QuizQuestion {
+
+    private String question;
+    private String answer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void printDetails() {
+        System.out.println(question);
+        System.out.println(answer);
+    }
+
 }
